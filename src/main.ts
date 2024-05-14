d54226d44d2efe62181043ad774c9b98f47a2215
Yhan17 <yhannunesleal@gmail.com>
2024-05-13 21:10:05 -0300

    chore: Update dependencies and add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('The URL Shortener API description')
+    .setVersion('0.0.1')
+    .addTag('urls')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
